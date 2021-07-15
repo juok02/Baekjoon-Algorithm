0fde7f1c24080200c6560bb07136b405519953ca
이주옥 <juok9802@gmail.com>
2021-07-16 00:28:49 +0900

    Create 1003_피보나치 함수.py

---
+++ StepUp/동적계획법/1003_피보나치 함수.py
@@ -0,0 +1,58 @@
+'''
+#문제
+fibonacci(3)을 호출하면 다음과 같은 일이 일어난다.
+
+fibonacci(3)은 fibonacci(2)와 fibonacci(1) (첫 번째 호출)을 호출한다.
+fibonacci(2)는 fibonacci(1) (두 번째 호출)과 fibonacci(0)을 호출한다.
+두 번째 호출한 fibonacci(1)은 1을 출력하고 1을 리턴한다.
+fibonacci(0)은 0을 출력하고, 0을 리턴한다.
+fibonacci(2)는 fibonacci(1)과 fibonacci(0)의 결과를 얻고, 1을 리턴한다.
+첫 번째 호출한 fibonacci(1)은 1을 출력하고, 1을 리턴한다.
+fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한다.
+
+1은 2번 출력되고, 0은 1번 출력된다. N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
+
+#입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. N은 40보다 작거나 같은 자연수 또는 0이다.
+
+#출력
+각 테스트 케이스마다 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력한다.
+
+
+
+ex)
+입력| 3
+   | 0
+   | 1
+   | 3
+
+출력| 1 0
+   | 0 1
+   | 1 2
+'''
+
+
+#code
+T = int(input())
+
+for i in range(T): #bottom-up
+  n = int(input())
+  d0=[0]*41
+  d1=[0]*41
+
+  d0[0]=1
+  d1[0]=0
+  d0[1]=0
+  d1[1]=1
+
+  for i in range(2,n+1):
+    d0[i]=d0[i-1]+d0[i-2]
+    d1[i]=d1[i-1]+d1[i-2]
+
+  print("%d %d" %(d0[n], d1[n]))
+
+
+
+
+    
