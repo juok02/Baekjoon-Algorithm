1e7027740bda4ed7a3a8bdbd1e111e501b51a217
이주옥 <juok9802@gmail.com>
2022-02-03 23:08:43 +0900

    Create 10872_팩토리얼.py

---
+++ StepUp/10재귀/10872_팩토리얼.py
@@ -0,0 +1,33 @@
+'''
+#문제
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+#입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+#출력
+첫째 줄에 N!을 출력한다.
+
+
+
+ex)
+입력| 10              | 0
+출력| 3628800         | 1
+
+'''
+
+
+#code
+def factorial(n):
+    result = 1
+    if n > 0 :
+        result = n * factorial(n-1)
+    return result
+
+n = int(input())
+print(factorial(n))
+
+
+
+
+    
